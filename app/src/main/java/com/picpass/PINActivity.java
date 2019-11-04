334b31a8855ae16e288673323d12a276f3d2392b
harrison <harrisonshelnutt@gmail.com>
2019-11-04 12:16:49 -0600

    added button to display keyboard manually

--- app/src/main/java/com/picpass/PINActivity.java
+++ app/src/main/java/com/picpass/PINActivity.java
@@ -2,11 +2,13 @@ package com.picpass;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.util.Log;
+import android.view.View;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 import android.widget.ImageView;
@@ -93,5 +95,10 @@ public class PINActivity extends AppCompatActivity {
         pinTextField.setText("");
     }
 
+    public void onShowKeyboard(View v) {
+        pinTextField.requestFocus();
+        InputMethodManager keyboardManager = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+        keyboardManager.showSoftInput(pinTextField, InputMethodManager.SHOW_IMPLICIT);
+    }
 
 }
